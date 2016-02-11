386284daa60c6cee5bd58af40e19e004f4e5fe3d
Colin Mullikin <ColinMullikin@Colins-MacBook-Pro.local>
2016-02-10 20:07:34 -0500

    Triangles and waves

--- 1.4.12-Challenge-SierpinskiTriangle/core/src/com/udacity/gamedev/sierpinskitriangle/SierpinskiTriangle.java
+++ 1.4.12-Challenge-SierpinskiTriangle/core/src/com/udacity/gamedev/sierpinskitriangle/SierpinskiTriangle.java
@@ -2,7 +2,10 @@ package com.udacity.gamedev.sierpinskitriangle;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /**
  * TODO: Start here
@@ -16,28 +19,55 @@ import com.badlogic.gdx.graphics.GL20;
 public class SierpinskiTriangle extends ApplicationAdapter {
 
     static final float SIZE = 10;
-    private static final int RECURSIONS = 7;
+    private static final int RECURSIONS = 4;
+
+    ShapeRenderer renderer;
+    FitViewport viewport;
 
     @Override
     public void create() {
-
-        }
+        renderer = new ShapeRenderer();
+        viewport = new FitViewport(SIZE,SIZE);
+    }
 
     @Override
     public void dispose() {
-
+        renderer.dispose();
     }
 
     @Override
     public void resize(int width, int height) {
-
+        viewport.update(width, height, true);
     }
 
     @Override
     public void render() {
-        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClearColor(1, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        viewport.apply();
+        renderer.setProjectionMatrix(viewport.getCamera().combined);
+        renderer.begin(ShapeRenderer.ShapeType.Filled);
+
+        renderer.setColor(Color.BLACK);
+        renderer.triangle(0,0,SIZE/2,SIZE,SIZE,0);
+
+        renderer.setColor(Color.WHITE);
+        drawTriangles(RECURSIONS,0,0,SIZE);
+
+        renderer.end();
+
+    }
+
+    public void drawTriangles(int recursions, float x, float y, float size){
+        if (recursions == 0){
+            return;
+        }
+        float newSize = size / 2;
+        renderer.triangle(x+newSize/2,y+newSize,x+size*3/4,y+newSize,x+newSize,y);
+        drawTriangles(recursions-1,x,y,newSize);
+        drawTriangles(recursions-1,x+newSize/2,y+newSize,newSize);
+        drawTriangles(recursions-1,x+newSize,y,newSize);
     }
 
 
--- 1.5.02-Exercise-ReciprocatingMotion/core/src/com/udacity/gamedev/reciprocatingmotion/ReciprocatingMotion.java
+++ 1.5.02-Exercise-ReciprocatingMotion/core/src/com/udacity/gamedev/reciprocatingmotion/ReciprocatingMotion.java
@@ -5,6 +5,8 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.utils.TimeUtils;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 
 /**
@@ -23,13 +25,14 @@ public class ReciprocatingMotion extends ApplicationAdapter {
     private static final float MOVEMENT_DISTANCE = WORLD_SIZE / 4;
 
     // TODO: Define a constant that fixes how long a cycle of the animation should take in seconds
+    private static final float PERIOD = 2;
 
 
     ShapeRenderer renderer;
     ExtendViewport viewport;
 
     // TODO: Create a long to hold onto ApplicationAdapter creation time
-
+    long creationTime;
 
     @Override
     public void create() {
@@ -37,7 +40,7 @@ public class ReciprocatingMotion extends ApplicationAdapter {
         viewport = new ExtendViewport(WORLD_SIZE, WORLD_SIZE);
 
         // TODO: Save current value of TimeUtils.nanoTime()
-
+        creationTime = TimeUtils.nanoTime();
     }
 
     @Override
@@ -65,21 +68,21 @@ public class ReciprocatingMotion extends ApplicationAdapter {
         float worldCenterY = viewport.getWorldHeight() / 2;
 
         // TODO: Figure out how long it's been since the animation started using TimeUtils.nanoTime()
+        long timeElapsedNano = TimeUtils.nanoTime() - creationTime;
 
 
         // TODO: Use MathUtils.nanoToSec to figure out how many seconds the animation has been running
-
+        float timeElapsedSec = MathUtils.nanoToSec * timeElapsedNano;
 
         // TODO: Figure out how many cycles have elapsed since the animation started running
-
+        float numCycles = timeElapsedSec / PERIOD;
 
         // TODO: Figure out where in the cycle we are
-
+        float partial = numCycles % 1;
 
         // TODO: Use MathUtils.sin() to set the x position of the circle
 
-
-        float x = worldCenterX;
+        float x = worldCenterX + MathUtils.sin(partial * MathUtils.PI2 )* MOVEMENT_DISTANCE;
         float y = worldCenterY;
         renderer.circle(x, y, CIRCLE_RADIUS);
         renderer.end();
